d8f53c069232743074fd115d4e315ee6349f1346
Wen Jiang <jiangwen84@gmail.com>
2016-07-08 10:08:34 -0600

    add ic

---
+++ modules/phase_field/src/ics/VesicleIC.C
@@ -0,0 +1,35 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#include "VesicleIC.h"
+
+template<>
+InputParameters validParams<VesicleIC>()
+{
+  InputParameters params = validParams<InitialCondition>();
+  params.addParam<Point>("center", Point(0,0,0), "The center coordinate");
+  params.addParam<Real>("epsilon", 0.01, "The interfacial penalty parameter."); 
+  return params;
+}
+
+VesicleIC::VesicleIC(const InputParameters & parameters) :
+    InitialCondition(parameters),
+    _center(parameters.get<Point>("center")),
+    _epsilon(getParam<Real>("epsilon"))
+{}
+
+Real
+VesicleIC::value(const Point & p)
+{
+  Real value = 0.0;
+
+  value = pow((p - _center).norm_sq(),0.5);
+
+  value = std::tanh(value/sqrt(2.0)/_epsilon);
+
+  return value;
+}
+
